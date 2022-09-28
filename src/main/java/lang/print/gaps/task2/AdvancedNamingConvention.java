edf4a7ca9fd74b35de7d17bf9759f64495b1fbd3
NURKEN <86570552+nurkenbai@users.noreply.github.com>
2022-09-28 16:01:26 +0500

    Task #2. Key Points test 2

--- src/main/java/lang/print/gaps/task2/AdvancedNamingConvention.java
+++ src/main/java/lang/print/gaps/task2/AdvancedNamingConvention.java
@@ -1,10 +1,10 @@
 package lang.print.gaps.task2;
 
 public class AdvancedNamingConvention {
-    private static final int adultAge = 18;
+    private static final int ADULT_AGE = 18;
 
     private int age;
-    private int phoneNumber;
+    private int phone_number;
 
     void callToFriend() {
         //some code
--- src/main/java/lang/print/gaps/task2/NamingConvention.java
+++ src/main/java/lang/print/gaps/task2/NamingConvention.java
@@ -3,13 +3,13 @@ package lang.print.gaps.task2;
 public class NamingConvention {
     public static void main(String[] args) {
         int variable = 10;
-        int variable_One = 1;
-        int variableTwo = 2;
+        int variable_one = 1;
+        int variable_two = 2;
         int variable_three = 3;
-        final int variableFour = 4;
-        final int variableFive = 5;
-        int variableSix = 6;
-        int variableSeven = 7;
-        int variableEight = 8;
+        final int  VARIABLE_FOUR = 4;
+        final int VARIABLE_FIVE = 5;
+        int variable_six = 6;
+        int variable_seven = 7;
+        int variable_eight = 8;
     }
 }
